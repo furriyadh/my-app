1884d53aae57fda2669702444b7ea775cbc1ce35
Hossam <ads@furriyadh.com>
2025-06-17 22:14:30 +0300

    Initial commit of my-site project

---
+++ .next/static/chunks/src_app_front-pages_contact_page_tsx_122dd083._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_01554ad6._.js"
+],
+    source: "dynamic"
+});
