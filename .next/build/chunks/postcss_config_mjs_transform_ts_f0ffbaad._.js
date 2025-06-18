9340a722c4358f5f446e865991cab382bcbc1e09
Hossam <ads@furriyadh.com>
2025-06-18 19:22:06 +0300

    Initial commit of my-site project

--- .next/build/chunks/postcss_config_mjs_transform_ts_f0ffbaad._.js
+++ .next/build/chunks/postcss_config_mjs_transform_ts_f0ffbaad._.js
@@ -6,8 +6,14 @@ var { g: global, __dirname } = __turbopack_context__;
 {
 __turbopack_context__.v((parentImport) => {
     return Promise.all([
-  "build/chunks/node_modules_b5d1def4._.js",
-  "build/chunks/[root-of-the-server]__05f88b00._.js"
+  "build/chunks/node_modules_postcss_lib_3559ed70._.js",
+  "build/chunks/node_modules_enhanced-resolve_lib_1ca5ea9f._.js",
+  "build/chunks/node_modules_jiti_dist_jiti_cjs_3caf0af2._.js",
+  "build/chunks/node_modules_jiti_dist_babel_cjs_fa930073._.js",
+  "build/chunks/node_modules_jiti_lib_jiti_mjs_6794adf4._.js",
+  "build/chunks/node_modules_tailwindcss_dist_d225bef2._.js",
+  "build/chunks/node_modules_b1456cfd._.js",
+  "build/chunks/[root-of-the-server]__1d18469c._.js"
 ].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
         return parentImport("[project]/postcss.config.mjs/transform.ts { CONFIG => \"[project]/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
     });
