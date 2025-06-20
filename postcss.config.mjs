7ba92330fa00b8c06c0a198ae0656bf01364f5a8
Hossam <ads@furriyadh.com>
2025-06-20 08:22:01 +0300

    Initial commit of my-site project

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    ("@tailwindcss/postcss"),
+    ("postcss-preset-env"),
+    ("@tailwindcss/nesting"),
+  ],
 };
 
 export default config;
